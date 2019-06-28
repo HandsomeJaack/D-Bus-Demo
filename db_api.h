7070b2824168de739e98000884d7f00c7ede720c
HandsomeJaack <doctor.dmitry99@gmail.com>
2019-06-28 14:15:51 +0300

    Formated code to Google style

--- db_api.h
+++ db_api.h
@@ -1,15 +1,15 @@
 #ifndef DBAPI_H
 #define DBAPI_H
 #ifdef __cplusplus
-extern "C"{
+extern "C" {
 #endif
 
 int delete_program(char* program);
-int add_program(char  *program, char *extension);
-char **select_program(char* file);
+int add_program(char* program, char* extension);
+char** select_program(char* file);
 int run(char* program, char* file);
 
-#ifdef __cplusplus 
+#ifdef __cplusplus
 }
 #endif
 #endif
